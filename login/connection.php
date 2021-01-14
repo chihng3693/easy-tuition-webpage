2b0ee0482b127dfbf81e90d3280ba9f11edef689
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-14 21:42:14 +0800

    Add files via upload

--- login/connection.php
+++ login/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbname = "easy-tuition";
+    $dbhost = "us-cdbr-east-03.cleardb.com";
+    $dbuser = "b674c79cb4cf70";
+    $dbpass = "8a7d3b35";
+    $dbname = "heroku_6aed07d9c46d786";
 
     $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
