12d2960a1831491928c12b92af24043579b50a40
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-16 16:33:23 +0800

    Add files via upload

--- login/loginMethod.php
+++ login/loginMethod.php
@@ -52,6 +52,10 @@
           } else if($_POST['userType'] === "tuitionCenter"){
             $sql = "SELECT * FROM tuition_centers WHERE tuitionEmail='$useremail' AND tuitionPassword='$password'";
 
+            if($sql) {
+              header("Location: loginPage.php?error=Incorrect Email or Password2");
+            }
+
             $result = mysqli_query($conn, $sql);
 
             if(mysqli_num_rows($result) === 1) {
@@ -64,7 +68,7 @@
                     header("Location: ../userhome/userhome.php");
                     exit();
                 } else {
-                    header("Location: loginPage.php?error=Incorrect Email or Password1");
+                    //header("Location: loginPage.php?error=Incorrect Email or Password1");
                     exit();
                 }
             } else {
