08ff82ce33d6336380ccc6af53cd6e733ae58e91
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-16 20:13:01 +0800

    Add files via upload

--- login/connection.php
+++ login/connection.php
@@ -2,17 +2,25 @@
 
     //Host server
 
-    $dbhost = "us-cdbr-east-03.cleardb.com";
-    $dbuser = "b674c79cb4cf70";
-    $dbpass = "8a7d3b35";
-    $dbname = "heroku_6aed07d9c46d786";
+    //get data from json
 
-    $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    $data = file_get_contents(__DIR__ . '/database.json');
+
+    $json = json_decode($data);
+
+    $conn = mysqli_connect($json->dbhost,$json->dbuser,$json->dbpass,$json->dbname);
 
     if(!$conn){
 
         die("failed to connect!");
     }
+
+    /*
+    $dbhost = "us-cdbr-east-03.cleardb.com";
+    $dbuser = "b674c79cb4cf70";
+    $dbpass = "8a7d3b35";
+    $dbname = "heroku_6aed07d9c46d786";
+    */
 
     //Local server
     /*
