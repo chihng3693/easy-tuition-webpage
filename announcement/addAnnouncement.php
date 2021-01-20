b83009a62818101d00e16f78e68f8f1ec6257522
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-20 22:21:52 +0800

    Add files via upload

--- announcement/addAnnouncement.php
+++ announcement/addAnnouncement.php
@@ -39,7 +39,7 @@
                   <?php
                     include("connection.php");
                     $sql = "SELECT classesName FROM tuition_class_bridge INNER JOIN tuition_classes
-                    WHERE tuition_class_bridge.tuitionID = '$_SESSION['userID']'";
+                    ON tuition_class_bridge.classesID = tuition_classes.classesID";
                     $result = ($conn, $sql);
 
                   ?>
