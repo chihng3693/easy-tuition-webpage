f763488d78bfc8a67f64282b73c35f2fe4c33c72
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-20 23:02:37 +0800

    Add files via upload

--- announcement/connection.php
+++ announcement/connection.php
@@ -1,9 +1,33 @@
 <?php
 
+    //Host server
+
+    //get data from json
+
+    $data = file_get_contents(__DIR__ . '/database.json');
+
+    $json = json_decode($data);
+
+    $conn = mysqli_connect($json->dbhost,$json->dbuser,$json->dbpass,$json->dbname);
+
+    if(!$conn){
+
+        die("failed to connect!");
+    }
+
+    /*
     $dbhost = "us-cdbr-east-03.cleardb.com";
     $dbuser = "b674c79cb4cf70";
     $dbpass = "8a7d3b35";
     $dbname = "heroku_6aed07d9c46d786";
+    */
+
+    //Local server
+    /*
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "easy-tuition";
 
     $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
@@ -11,5 +35,6 @@
 
         die("failed to connect!");
     }
+    */
 
 ?>
