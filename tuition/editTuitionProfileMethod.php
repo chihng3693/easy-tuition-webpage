71b0e56d7c8068284ef2c2d3e3e7c1431e4b6549
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-21 00:49:05 +0800

    Add files via upload

--- tuition/editTuitionProfileMethod.php
+++ tuition/editTuitionProfileMethod.php
@@ -2,7 +2,8 @@
     session_start();
     include("connection.php");
 
-    if(isset($_POST['Tuiname']) && isset($_POST['phone']) && isset($_POST['street']) && isset($_POST['poscode']) && isset($_POST['city']) && isset($_POST['state'])){
+    if(isset($_POST['Tuiname']) && isset($_POST['phone'])
+    && isset($_POST['street']) && isset($_POST['poscode']) && isset($_POST['city']) && isset($_POST['state'])){
 
         function validate($data){
             $data = trim($data);
@@ -18,10 +19,25 @@
         $tposcode = validate($_POST['poscode']);
         $tcity = validate($_POST['city']);
         $tstate = validate($_POST['state']);
-        $tuitionID = $_SESSION('userID');
+        $tuitionID = $_SESSION['userID'];
 
-        echo "asdasd";
+        if(empty($tname) || empty($tphone)  || empty($tstreet)
+            || empty($tposcode) || empty($tcity) || empty($tstate)){
+            header("Location: editTuitionProfile.php?error=Cannot Leave any Field Blank");
+            exit();
+        } else {
+            $sqlpush = "UPDATE tuition_centers SET tuitionName='$tname', tuitionPhone='$tphone', tuitionStreet='$tstreet', tuitionPoscode='$tposcode', tuitionCity='$tcity', tuitionState='$tstate'
+            WHERE tuitionID='$tuitionID'";
+            $resultpush = mysqli_query($conn, $sqlpush);
 
+            if($resultpush){
+                header("Location: editTuitionProfile.php?success=Account has been created!");
+                exit();
+            } else{
+                header("Location: editTuitionProfile.php?error=Unknown error occurred");
+                exit();
+            }
+        }
     }
     else {
         header("Location: editTuitionProfile.php");
