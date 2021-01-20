8fc0b3ff8ad0ed9ee2fc41fa4765c6e427f1e5a4
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-20 22:06:11 +0800

    Add files via upload

--- announcement/addAnnouncement.php
+++ announcement/addAnnouncement.php
@@ -36,7 +36,16 @@
             <div class="feedback">
               <p>Subject:
                 <select id="subject" name="subject">
-
+                  <?php
+                    include("connection.php");
+                    $sql = mysqli_query($conn, "SELECT classesID FROM tuition_class_bridge WHERE tuitionID = '$_SESSION['userID']' ");
+                    while ($row = $sql->fetch_assoc()){
+                        $classesID = $row['classesID'];
+                        $query = mysqli_query($conn, "SELECT classesName FROM tuition_classes WHERE classesID = '$classesID' ");
+                        $row1 = mysqli_fetch_assoc($query);
+                        echo "<option value="'.$classesID.'">" '.$row1['classesName'].' "</option>";
+                    }
+                  ?>
                 </select>
               </p>
               <p>Annoucement Notice</p>
