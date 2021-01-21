7367d84c0dea44977c95f8f402fc208cf6d8cdaf
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-21 09:43:37 +0800

    Add files via upload

--- tuition/editTuitionProfile.php
+++ tuition/editTuitionProfile.php
@@ -15,7 +15,7 @@
                   <li><img src="../images/tuition-logo.png" alt="tuition-logo" class="logo"></li>
                   <li> <a href="tuitionhome.php">Home</a> </li>
                   <li> <a href="../announcement/addAnnouncement.php">Add Announcement</a> </li>
-                  <li> <a href="../payment/payment.php">Payment</a> </li>
+                  <li> <a href="viewPayment.php">Payment</a> </li>
                   <li> <a href="../tuition/addClass.php">Add Class</a> </li>
                   <li> <a href="../tuition/editTuitionProfile.php">Edit Profile</a> </li>
                 </ul>
