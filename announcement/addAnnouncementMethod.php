081e07a80162305efef070fb230d9d12218b5511
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-23 23:52:57 +0800

    Add files via upload

--- announcement/addAnnouncementMethod.php
+++ announcement/addAnnouncementMethod.php
@@ -28,14 +28,10 @@
                 $announcementID = mysqli_insert_id($conn);
 
                 if($resultpush){
-
-                    echo $announcementID;
-                    echo $classesID;
-
                     $queryGetUID = "SELECT * FROM user_class_bridge WHERE classesID='$classesID'";
                     $queryGetUID = mysqli_query($conn, $queryGetUID);
                     $rows = mysqli_num_rows($queryGetUID);
-                    
+
                     if($rows > 0){
 
                         echo "yes";
@@ -43,17 +39,14 @@
                         while($rows = mysqli_fetch_assoc($queryGetUID)) {
                             $userID = $rows['userID'];
 
-                            echo "yes";
-                            echo $userID;
-
-                            $sql = "INSERT INTO announcementbridge(announcementID, userID, classesID) 
+                            $sql = "INSERT INTO announcementbridge(announcementID, userID, classesID)
                             VALUES('$announcementID', '$userID', '$classesID')";
                             $result = mysqli_query($conn, $sql);
                         }
 
                         header("Location: ../tuition/tuitionhome.php");
                         exit();
-                        
+
                     }
 
                 } else{
