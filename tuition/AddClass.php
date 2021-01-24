cfb2b4a7a98ce750e75c8bc2168194b0a384688c
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-01-24 12:22:31 +0800

    Add files via upload

--- tuition/AddClass.php
+++ tuition/AddClass.php
@@ -40,10 +40,18 @@
         <input type="text" name="endtime" placeholder="End Time">
         <br>
         <label>Class Day</label>
-        <input type="text" name="cday" placeholder="Day">
+        <select id="cday" name="cday">
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+            <option value="Sunday">Sunday</option>
+        </select>
         <br>
         <label>Class Price</label>
-        <input type="text" name="cprice" placeholder="Price">
+        <input type="text" name="cprice" placeholder="Price (Input with currency)">
         <br>
         <label>Class Teacher</label>
         <input type="text" name="cteacher" placeholder="Teacher">
